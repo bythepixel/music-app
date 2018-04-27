98a0c686174ba69a9f236faa1ac95ad5871fd431
Elliott <elliott@elliottregan.com>
2018-04-27 13:13:31 -0600

    core layout

--- resources/assets/js/components/Core.vue
+++ resources/assets/js/components/Core.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <Searchbar />
-    <PlayerControls />
-    <Playlist />
+  <div class="app-layout">
+    <Searchbar class="header" />
+    <Playlist class="main-view" />
+    <PlayerControls class="controls" />
   </div>
 </template>
 
@@ -24,5 +24,29 @@ export default {
 </script>
 
 <style scoped>
+
+  .app-layout {
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: max-content 1fr max-content;
+    grid-template-areas: "header"
+                         "main-view"
+                         "controls";
+  }
+
+  .header {
+    grid-area: "header";
+  }
+
+  .main-view {
+    grid-area: "main-view";
+    overflow: scroll;
+  }
+
+  .controls {
+    grid-area: "controls";
+  }
 
 </style>
